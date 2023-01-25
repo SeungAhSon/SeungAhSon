93318e293cb0f86087c0d4476228b7f84eab5977
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2023-01-25 11:46:41 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,16 +1,6 @@
-#Enter your code here
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = agedef say_hello(self):
+        print("Hello, my name is {} and I am {} years old".format(self.name, self.age))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Enter your code here
